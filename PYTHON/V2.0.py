4eab50bb968f8f31869fb8acd86dd2ccd47eef4e
lyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <ezmydhl@outlook.com>
2026-02-16 17:39:37 +0800

    Add files via upload
    V2.0修复了一些些BUG🤷‍♂️🤷‍♂️
    V2.1添加了两个 ‘break’ 退出循环

---
+++ PYTHON/V2.0.py
@@ -0,0 +1,27 @@
+
+A=input("  ")
+V=input("  ")
+numberoftimes=0
+C=int(A)
+D: int=int(V)
+for i in range(100):
+    numberoftimes+=1
+    if 'C+D=0':
+        if 'numberoftimes>1':
+            print("is not")
+            print("but")
+            print(C+"is it")
+            break                  # ：（
+        if 'numberoftimes=1':
+          print("goodjob")
+          break
+C=C+1
+#lll              yyy       yyy   yyy       yyy
+#lll               yyy     yyy     yyy     yyy
+#lll                yyy   yyy       yyy   yyy
+#lll                 yyyyyyy         yyyyyyy
+#lll                  yy-yy           yy-yy
+#lll                   yyy             yyy
+#llllllllllllll        yyy             yyy
+#llllllllllllll        yyy             yyy
+
